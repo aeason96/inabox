2fb95d75296ecdd5be88e3ee2e2b56cd70999bc5
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-05-02 19:49:27 -0400

    stub

--- app/src/main/java/com/example/andrew/inabox/fragments/AnswerQuestionFragment.java
+++ app/src/main/java/com/example/andrew/inabox/fragments/AnswerQuestionFragment.java
@@ -1,6 +1,13 @@
 package com.example.andrew.inabox.fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.andrew.inabox.Game;
+import com.example.andrew.inabox.R;
 
 /**
  * Created by kevincianfarini on 5/2/17.
@@ -10,4 +17,14 @@ public class AnswerQuestionFragment extends Fragment {
 
     public static final String TAG_ANSWER_QUESTION_FRAGMENT = "answer_question_fragment";
 
+    private Game game;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        game = (Game) getActivity();
+        View view = inflater.inflate(R.layout.fragment_answer_question, container, false);
+        return view;
+    }
 }
