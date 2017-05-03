485575933bcfae80ee91ca0fa2b5e6ee3b4ad5de
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-05-03 17:57:16 -0400

    working

--- app/src/main/java/com/example/andrew/inabox/fragments/AnswerWaitFragment.java
+++ app/src/main/java/com/example/andrew/inabox/fragments/AnswerWaitFragment.java
@@ -1,6 +1,28 @@
 package com.example.andrew.inabox.fragments;
 
 import android.app.Fragment;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.example.andrew.inabox.Game;
+import com.example.andrew.inabox.R;
+import com.example.andrew.inabox.models.Constants;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 /**
  * Created by kevincianfarini on 5/3/17.
@@ -9,5 +31,84 @@ import android.app.Fragment;
 public class AnswerWaitFragment extends Fragment {
 
     public static final String TAG_ANSWER_WAIT_FRAGMENT = "tag_answer_wait_fragment";
+
+
+    private Game game;
+    private TextView remainingPlayers;
+    private Thread pollThread;
+    private Handler handler;
+    int totalPlayers;
+    int answeredPlayers;
+
+    int questionID;
+
+
+    public AnswerWaitFragment() {
+
+    }
+
+    public static AnswerWaitFragment newInstance() {
+        return new AnswerWaitFragment();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        game = (Game) getActivity();
+        View view = inflater.inflate(R.layout.fragment_answer_wait, container, false);
+
+        remainingPlayers = (TextView) view.findViewById(R.id.remaining_players);
+
+        new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                String url = Constants.BASE_URL + "question/" + game.getGameRoom().gameID;
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            questionID = response.getInt("id");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        //
+                    }
+                });
+                game.getRequestQueue().add(request);
+                return null;
+            }
+        };
+
+        return view;
+
+    }
+
+
+    public void pollForPlayers() {
+        handler = new Handler(Looper.getMainLooper()) {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 0) {
+                    remainingPlayers.setText(String.format("%d players haven't answered yet", totalPlayers - answeredPlayers));
+                    if (totalPlayers - answeredPlayers == 0) {
+                        //everyone answered TODO change fragment
+                    }
+                }
+            }
+        };
+
+        pollThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                String url = Constants.BASE_URL + "answers/" + 
+            }
+        })
+
+    }
 
 }
