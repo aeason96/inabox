4517c983398d0c349d8a3f106db0915c257a3d82
Kevin Cianfarini <kevincianfarini@gmail.com>
2017-05-03 11:08:34 -0400

    stub for answer question fragment

--- app/src/main/java/com/example/andrew/inabox/fragments/AnswerQuestionFragment.java
+++ app/src/main/java/com/example/andrew/inabox/fragments/AnswerQuestionFragment.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.example.andrew.inabox.Game;
 import com.example.andrew.inabox.R;
@@ -18,6 +20,8 @@ public class AnswerQuestionFragment extends Fragment {
     public static final String TAG_ANSWER_QUESTION_FRAGMENT = "answer_question_fragment";
 
     private Game game;
+    private EditText answer;
+    private TextView question;
 
     public AnswerQuestionFragment(){
 
@@ -34,6 +38,11 @@ public class AnswerQuestionFragment extends Fragment {
                              Bundle savedInstanceState) {
         game = (Game) getActivity();
         View view = inflater.inflate(R.layout.fragment_answer_question, container, false);
+
+
+        answer = (EditText) view.findViewById(R.id.answer_edit_text);
+        question = (TextView) view.findViewById(R.id.question_text);
+
         return view;
     }
 }
